8e64a9cd1ad6ae10b1778db23885be5c0363e6b6
Alexey Feklyunin <ninetailed1995@gmail.com>
2023-04-12 23:34:00 +0400

    feat: add match type

--- src/types.ts
+++ src/types.ts
@@ -17,3 +17,22 @@ export type GeneralProfile = {
   rank_tier: number,
   profile: ShortProfile,
 }
+
+export type Match = {
+  match_id: number,
+  player_slot: number,
+  radiant_win: boolean,
+  duration: number,
+  game_mode: number,
+  lobby_type: number,
+  hero_id: number,
+  start_time: number,
+  version: number,
+  kills: number,
+  deaths: number,
+  assists: number,
+  skill: number,
+  average_rank: number,
+  leaver_status: number,
+  party_size: number
+}
