8d87ba040705dcedcedb1f63e9ed1080511a6ded
Alexey Feklyunin <ninetailed1995@gmail.com>
2023-04-15 01:39:24 +0400

    feat: add links to profiles on another resources

--- src/components/ui/Profile/General/General.tsx
+++ src/components/ui/Profile/General/General.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import styles from "./General.module.scss";
 import { GeneralProfile } from "@/types";
+import Rank from "../Rank/Rank";
+import Link from "next/link";
 
 type Props = {
   data: GeneralProfile;
@@ -10,15 +12,43 @@ const General = ({ data }: Props) => {
   return (
     <div className={styles.general}>
       <div>
-        <p>{data.rank_tier}</p>
-        <Image
-          className={styles.avatar}
-          src={data.profile.avatarfull}
-          width={64}
-          height={64}
-          alt=""
-        ></Image>
-        <p className={styles.name}>{data.profile.personaname}</p>
+        <div>
+          <Rank rank={data.rank_tier} />
+          <Image
+            className={styles.avatar}
+            src={data.profile.avatarfull}
+            width={64}
+            height={64}
+            alt=""
+          ></Image>
+          <p className={styles.name}>{data.profile.personaname}</p>
+        </div>
+        <div>
+          <Link
+            href={`https://www.dotabuff.com/players/${data.profile.account_id}`}
+            passHref
+            target="_blank"
+          >
+            <Image
+              src="/icons/dotabuff.ico"
+              width={18}
+              height={18}
+              alt="Dotabuff profile"
+            />
+          </Link>
+          <Link
+            href={`https://steamcommunity.com/profiles/${data.profile.steamid}`}
+            passHref
+            target="_blank"
+          >
+            <Image
+              src="/icons/steam.ico"
+              width={18}
+              height={18}
+              alt="Steam profile"
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
